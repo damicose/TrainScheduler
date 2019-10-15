86687ccc37579b389c82d63f7f89dac6cfe498be
Sonny D <damicose14@gmail.com>
2019-10-15 11:23:49 -0500

    basic directory setup and initial js work

---
+++ public/js/function.js
@@ -0,0 +1,6 @@
+// Currently setting up as if operating purely locally, will need to rejigger for localForage!!
+
+let trainName = document.getElementById("formTrainName").value.trim();
+let trainDest = document.getElementById("formDestination").value.trim();
+let trainTime = document.getElementById("formTime").value.trim();
+let trainFreq = document.getElementById("formFrequency").value.trim();
