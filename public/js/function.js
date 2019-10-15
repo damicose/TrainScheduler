34270f5f67c55c31bd1921ec010cd860019a13b3
Sonny D <damicose14@gmail.com>
2019-10-15 12:00:20 -0500

    still fleshing out functionality

--- public/js/function.js
+++ public/js/function.js
@@ -1,6 +1,27 @@
-// Currently setting up as if operating purely locally, will need to rejigger for localForage!!
+// Store train info as object? Array? LF is flexible. Not sure about frequency formatting--!
+// Array of objects maybe
+
+const trainArr = [];
+
+const sample = {
+    name: "The Sample Express",
+    dest: "Omaha, NE",
+    time: 1245,
+    freq: 0100
+};
+
+document.getElementById("submitBtn").addEventListener("click", function(){
+
+// Currently set up as if operating purely locally, will need to rejigger for localForage!!
 
 let trainName = document.getElementById("formTrainName").value.trim();
 let trainDest = document.getElementById("formDestination").value.trim();
 let trainTime = document.getElementById("formTime").value.trim();
 let trainFreq = document.getElementById("formFrequency").value.trim();
+
+// Create the th and td elements for each object in the train array && (stored and called ~foragely~)
+// Need to refresh on LF, but may not even need to loop? 
+})
+
+// Looks as if will need for loop to iterate through? 'th scope="row"' i++ ?? Otherwise ugly hard code?
+
