5dd4d3199a66fd3db3e083da17306effc4a62b9f
Sonny D <damicose14@gmail.com>
2019-10-25 02:47:03 -0500

    tweaking form input for time, console log for output format

--- public/js/function.js
+++ public/js/function.js
@@ -1,32 +1,42 @@
 // Crafted with guidance from Section 07-09
 
 function setTrain(newTrain, cb) {
-    localforage.setItem("trains", newTrain).then(function() {
+    localforage.setItem("trains", newTrain).then(function () {
         cb();
     });
 };
 
 function getTrains(cb) {
-    localforage.getItem("trains").then(function(results) {
+    localforage.getItem("trains").then(function (results) {
         cb(results || [])
     });
 };
 
 function handleTrain(newName, newDest, newTime, newFreq) {
-    getTrains(function(trainArr) {
+    getTrains(function (trainArr) {
         trainArr.push({
             name: newName,
             dest: newDest,
             time: newTime,
             freq: newFreq
         });
-        setTrain(trainArr)
+        setTrain(trainArr, updateTrain);
+    });
+}
+
+function storeTrain() {
+    document.getElementById("submitBtn").addEventListener("click", function () {
+
+        let trainName = document.getElementById("formTrainName").value;
+        let trainDest = document.getElementById("formDestination").value;
+        let trainTime = document.getElementById("formTime").value;
+        let trainFreq = document.getElementById("formFrequency").value;
+        handleTrain(trainName, trainDest, trainTime, trainFreq);
     })
 }
 
-
-// Store train info as object? Array? LF is flexible. Not sure about frequency formatting--!
-// Array of objects maybe
+// Actually call it
+storeTrain();
 
 const trainArr = [];
 
@@ -37,17 +47,6 @@ const sample = {
     freq: 0100
 };
 
-document.getElementById("submitBtn").addEventListener("click", function(){
 
-// Currently set up as if operating purely locally (?), will need to rejigger for localForage!!
-
-let trainName = document.getElementById("formTrainName").value.trim();
-let trainDest = document.getElementById("formDestination").value.trim();
-let trainTime = document.getElementById("formTime").value.trim();
-let trainFreq = document.getElementById("formFrequency").value.trim();
-
-// Create the th and td elements for each object in the train array && (stored and called ~foragely~)
-// Need to refresh on LF, but may not even need to loop? --->LF does have an iterate method!
-})
 
 // Looks as if will need for loop to iterate through? 'th scope="row"' i++ ?? Otherwise ugly hard code?
